d2687dc6e5a03ae64bdf4494148616379d22cf6e
millerjacob <jmmiller95616@gmail.com>
2019-07-09 14:59:06 -0700

    added accessibility of resonator state variables and properties

--- pmmcontrol/simulator/detector.py
+++ pmmcontrol/simulator/detector.py
@@ -1,5 +1,4 @@
 from pmmcontrol.simulator.hysteresis import Hysteresis
-from scipy.constants import mu_0, pi
 import numpy as np
 
 class Detector():
@@ -11,12 +10,12 @@ class Detector():
         self.col_currents = np.zeros(cols)
 
         #arbitrary matrix of evenly spaced frequencies
-        self.fstart = 0 #MHz
-        self.fstop = 20 #MHz
-        self.frequencies = np.linspace(self.fstart, self.fstop, self.rows*self.cols).reshape(self.rows, self.cols)
+        self.fstart = 1 #MHz
+        self.fstop = 3 #MHz
+        self.baseFrequencies = np.linspace(self.fstart, self.fstop, self.rows*self.cols).reshape(self.rows, self.cols)
 
         self.resArray = np.empty((self.rows, self.cols), dtype=object)
-        self.resArray[:,:] = np.vectorize(Resonator)(self.frequencies)
+        self.resArray[:,:] = np.vectorize(Resonator)(self.baseFrequencies)
 
     def setRowCurrent(self, I, row):
         self.row_currents[row] = I
@@ -36,15 +35,11 @@ class Detector():
                 self.resArray[row, col].setCurrent(self.row_currents[row] + self.col_currents[col])
 
 class Resonator(Hysteresis):
-    def __init__(self, baseFreq, N=200, satField=0.0025, satMag=0.5,
-                 remanence=0, coercivity=0.002):
+    def __init__(self, baseFreq, N=200, satField=25, satMag=0.5,
+                 remanence=0, coercivity=20):
 
         #Resonator properties
         self.__baseFreq = baseFreq
-        self.__nLoops = 10
-        self.__loopRadius = 0.0001
-        self.__distLoopMag = 0
-
         self.__satMag = satMag
         self.__remanence = remanence
         self.__coercivity = coercivity
@@ -61,7 +56,7 @@ class Resonator(Hysteresis):
         self.__size = N
 
         Hysteresis.__init__(self, self.__satField, self.__satMag, self.__coercivity,
-                            self.__remanence, self.__size, 'Applied Field (T)', 'Magnetization (T)')
+                            self.__remanence, self.__size, 'Applied Field (G)', 'Magnetization (T)')
 
     def setCurrent(self, I):
 
@@ -77,23 +72,45 @@ class Resonator(Hysteresis):
 
         self.__deltaFreq = self.__fieldToDeltaFreq(self.__B_res)
 
-        self.__freq = self.__freq + self.__deltaFreq
+        self.__freq = self.__baseFreq + self.__deltaFreq
 
         stateData = {'Current': self.__current,
-                     'Exterior Field': self.__B_ext,
+                     'ExtField': self.__B_ext,
                      'Magnetization': self.__mag,
-                     'Resonator Field': self.__B_res,
-                     'Shift in Frequency': self.__deltaFreq,
-                     'Current frequency': self.__freq}
+                     'ResField': self.__B_res,
+                     'DeltaFrequency': self.__deltaFreq,
+                     'Frequency': self.__freq}
 
         return stateData
 
-    def __currToField(self, I): #Amps -> Tesla
-        return (0.001) * (I / 0.0025)
+    def __currToField(self, I): #Amps -> Gauss
+        return (10) * (I / 0.0025)
 
     def __magToField(self, M): #Tesla -> Tesla
         return (0.00021) * (M / 0.5)
 
     def __fieldToDeltaFreq(self, B): #Tesla -> MHz
         slope = -(.08)/(0.0003**2)
-        return slope * B
+        return slope * (B + .00015)**2
+
+    @property
+    def state(self):
+        stateData = {'Current': self.__current,
+                     'ExtField': self.__B_ext,
+                     'Magnetization': self.__mag,
+                     'ResField': self.__B_res,
+                     'DeltaFrequency': self.__deltaFreq,
+                     'Frequency': self.__freq}
+
+        return stateData
+
+    @property
+    def properties(self):
+        propertyData = {'BaseFrequnecy': self.__baseFreq,
+                      'SatField': self.__satField,
+                      'SatMagnetization': self.__satMag,
+                      'Remanence': self.__remanence,
+                      'Coercivity': self.__coercivity}
+
+        return propertyData
+
